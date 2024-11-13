1d72fc52ce39e8019cf5ebb5cef7d8fcfaea5ed6
Niraleehirpara2002 <hirpara.ni@northeastern.edu>
2024-11-13 11:08:45 -0500

    Lab 7

---
+++ Lab7/Lab7_JDBC/src/utility/DatabaseQueries.sql
@@ -0,0 +1,15 @@
+show databases
+
+CREATE DATABASE medicaldb;
+use medicaldb;
+
+create table patient (id int PRIMARY KEY auto_increment, 
+fname varchar(100), lname varchar(100), age int,
+email varchar(100), message varchar(100),
+gender varchar(10), patient_type varchar(50)); 
+
+insert into patient(fname, lname, age, email, message, gender, patient_type) values ();
+
+delete from patient where id=1;
+
+select * from patient;
