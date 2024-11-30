81635466dd474a5bac11a69b1f1f61f33bd4da26
Niraleehirpara2002 <hirpara.ni@northeastern.edu>
2024-11-29 23:44:15 -0500

    Lab8

---
+++ Assignment5/Assignment5/src/ThreadsInJava.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author NIRAV
+ */
+public class ThreadsInJava {
+        public static void main(String[] args){
+        Thread1 t1 = new Thread1();
+        t1.start();
+        
+        Thread2 t2 = new Thread2();
+        t2.start();
+        
+        TwinkleTwinkle t3 = new TwinkleTwinkle();
+        t3.start();
+    }
+}
